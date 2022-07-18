94da3c96f78aeb0ec7e9440966907db91b4ea7c7
Anupam Mazumdar <anupam.mazumdar@live.com>
2022-07-19 00:03:06 +0530

    Update constexptr.cpp

--- constexptr.cpp
+++ constexptr.cpp
@@ -1,16 +1,20 @@
+using namespace std;
 
-#include <random>
-
-constexpr int getRandomNo(int a)
+constexpr int add10(int a)
 {
   return a + 10;
 }
 
 int main()
 {
-    int a;
-    const int varB = getRandomNo(a);       // OK
-    constexpr int varC = getRandomNo(10);   // not OK! compilation error
+    int a = 50;
+
+    const int varB = add10(a);       // not OK! compilation error
+    constexpr int varC = add10(10);   // not OK!
+    constexpr int varD = add10(30);   // not OK!
+    
+    // static_assert(varB > varC, "Should not happen"); // compile error as non constant condtion used i.e. varB
+    static_assert(varD > varC, "Should not happen"); // Compile time error if varD < varC
 
     return 0;
 }
